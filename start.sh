017fc4ed85087664b1574d50066356f94ce4be0d
ᴢᴇɴσᴠ <143601779+RealZenX@users.noreply.github.com>
2023-10-14 12:51:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @TeamSugoiX
+# Subscribe YouTube Channel For Amazing Bot @team_netflix
+# Ask Doubt on telegram @veldxd
 
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/RealZenX/Lucy_new.git /Lucy_new
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Lucy_new
 fi
-cd /VJ-FILTER-BOT 
+cd /Lucy_new
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
